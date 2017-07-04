92ef24cef5190e59d269498e0aaf2dc56bbbcdb0
Jay Connor <mjzeel@gmail.com>
2017-07-04 13:42:04 +0530

    ngx-charts module added

--- src/app/credit-term-spread-summary/credit-term-spread-summary.component.ts
+++ src/app/credit-term-spread-summary/credit-term-spread-summary.component.ts
@@ -1,15 +1,101 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-credit-term-spread-summary',
-  templateUrl: './credit-term-spread-summary.component.html',
-  styleUrls: ['./credit-term-spread-summary.component.css']
+	selector: 'app-credit-term-spread-summary',
+	templateUrl: './credit-term-spread-summary.component.html',
+	styleUrls: ['./credit-term-spread-summary.component.css']
 })
 export class CreditTermSpreadSummaryComponent implements OnInit {
 
-  constructor() { }
+	single: any[];
+	multi: any[];
 
-  ngOnInit() {
-  }
+	view: any[] = [500, 200];
 
+	 // options
+	showXAxis = true;
+	showYAxis = true;
+	gradient = false;
+	showLegend = true;
+	showXAxisLabel = true;
+	xAxisLabel = 'Country';
+	showYAxisLabel = true;
+	yAxisLabel = 'Population';
+	
+	colorScheme = {
+		domain: [
+			'#A133D5', '#DD3F5C', '#1ABB9C'
+		]
+	};
+
+	onSelect(event) {
+		console.log(event);
+	}
+
+	constructor() {
+		Object.assign(this, { single });
+	}
+
+	ngOnInit() {
+
+	}
 }
+
+export var single = [
+	{
+		"name": "Germany",
+		"value": 8940000
+	},
+	{
+		"name": "USA",
+		"value": 5000000
+	},
+	{
+		"name": "France",
+		"value": 7200000
+	}
+];
+
+export var multi = [
+	{
+		"name": "Germany",
+		"series": [
+			{
+				"name": "2010",
+				"value": 7300000
+			},
+			{
+				"name": "2011",
+				"value": 8940000
+			}
+		]
+	},
+
+	{
+		"name": "USA",
+		"series": [
+			{
+				"name": "2010",
+				"value": 7870000
+			},
+			{
+				"name": "2011",
+				"value": 8270000
+			}
+		]
+	},
+
+	{
+		"name": "France",
+		"series": [
+			{
+				"name": "2010",
+				"value": 5000002
+			},
+			{
+				"name": "2011",
+				"value": 5800000
+			}
+		]
+	}
+];	
