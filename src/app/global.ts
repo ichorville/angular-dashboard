e504c5f7af77f7537fd628eff7fbf7bc410a1eb4
Jay Connor <mjzeel@gmail.com>
2017-07-06 15:45:15 +0530

    file updated

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,4 +1,3 @@
 export const GlobalVariable = Object.freeze({
-	// BASE_API_URL: 'http://220.247.228.51/VantageDashboardAPI/api/home'
-	BASE_API_URL: 'http://www.axienta.lk/VantageDashboardAPI/api/home'
+	BASE_API_URL: 'http://220.247.228.51/VantageDashboardAPI/api/home'
 });
